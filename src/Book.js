a0a47c6b70f3f6c39c580a5f2212c860742af537
Matthew Miller <mmlak_0109@yahoo.com>
2021-02-12 21:45:57 +0900

    Removed unnecessary prop

--- src/Book.js
+++ src/Book.js
@@ -2,7 +2,7 @@ import React from 'react'
 import BookShelfChanger from './BookShelfChanger'
 
 const Book = props => {
-  const {book, shelf, onShelfChange} = props;
+  const {book, onShelfChange} = props;
 
   return (
     <li>
